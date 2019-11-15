0edb34cdc57724f97c9d769e1952004564e42588
sslavik <sslavikshy@gmail.com>
2019-11-15 11:51:54 +0100

    Moddified and Finished StaticFragment

--- app/src/main/java/com/sslavik/staticfragment/MainActivity.java
+++ app/src/main/java/com/sslavik/staticfragment/MainActivity.java
@@ -14,16 +14,19 @@ public class MainActivity extends AppCompatActivity implements FragmentA.OnFragm
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // BUSCAMOS LA VISTA DEL FRAGMENT
+        fragmentB = (FragmentB) getSupportFragmentManager().findFragmentById(R.id.fragmentb);
         Log.d(TAG, "Activity -> onCreate()");
     }
 
     @Override
     public void onSetTextSize(int size) {
-
+        // La activity puede realizar cualquier operacion o comprobacion de los datos antes de pasárselo al Fragment --> Clase Controladora
+        fragmentB.setMessageSize(size);
     }
 
     @Override
     public void onClickTextChange(String text) {
-
+        fragmentB.setTextMessage(text);
     }
 }
